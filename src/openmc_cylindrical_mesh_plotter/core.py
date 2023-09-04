b75746c492c4de3c640e182fba6497fbf3bcd150
Jonathan Shimwell <mail@jshimwell.com>
2023-09-04 15:09:22 +0100

    fixed aspect ratio

--- src/openmc_cylindrical_mesh_plotter/core.py
+++ src/openmc_cylindrical_mesh_plotter/core.py
@@ -17,14 +17,11 @@ if version.parse(openmc.__version__) < version.parse("0.13.3"):
     )
     raise ValueError(msg)
 
-_BASES = ["rz", "phiz"]
-
 _default_outline_kwargs = {"colors": "black", "linestyles": "solid", "linewidths": 1}
 
 
-def plot_mesh_tally(
+def plot_mesh_tally_rz_slice(
     tally: "openmc.Tally",
-    basis: str = "rz",
     slice_index: Optional[int] = None,
     score: Optional[str] = None,
     axes: Optional[str] = None,
@@ -33,7 +30,7 @@ def plot_mesh_tally(
     outline: bool = False,
     outline_by: str = "cell",
     geometry: Optional["openmc.Geometry"] = None,
-    geometry_basis: 'xy',
+    geometry_basis: str = "xz",
     pixels: int = 40000,
     colorbar: bool = True,
     volume_normalization: bool = True,
@@ -47,8 +44,6 @@ def plot_mesh_tally(
     ----------
     tally : openmc.Tally
         The openmc tally to plot. Tally must contain a MeshFilter that uses a CylindricalMesh.
-    basis : {'rz', 'phiz'}
-        The basis directions for the plot
     slice_index : int
         The mesh index to plot
     score : str
@@ -67,6 +62,8 @@ def plot_mesh_tally(
         Indicate whether the plot should be colored by cell or by material
     geometry : openmc.Geometry
         The geometry to use for the outline.
+    geometry_basis : str
+        The axis to use for the geometry slice
     pixels : int
         This sets the total number of pixels in the plot and the number of
         pixels in each basis direction is calculated from this total and
@@ -89,7 +86,7 @@ def plot_mesh_tally(
         Resulting image
     """
 
-    cv.check_value("basis", basis, _BASES)
+    cv.check_value("geometry_basis", geometry_basis, ["xz", "yz"])
     cv.check_value("axis_units", axis_units, ["km", "m", "cm", "mm"])
     cv.check_type("volume_normalization", volume_normalization, bool)
     cv.check_type("outline", outline, bool)
@@ -99,9 +96,9 @@ def plot_mesh_tally(
         raise NotImplemented(
             f"Only CylindricalMesh are currently supported not {type(mesh)}"
         )
-    # if mesh.n_dimension != 3:
-    #     msg = "Your mesh has {mesh.n_dimension} dimension and currently only CylindricalMesh with 3 dimensions are supported"
-    #     raise NotImplementedError(msg)
+    if mesh.n_dimension != 3:
+        msg = "Your mesh has {mesh.n_dimension} dimension and currently only CylindricalMesh with 3 dimensions are supported"
+        raise NotImplementedError(msg)
 
     # if score is not specified and tally has a single score then we know which score to use
     if score is None:
@@ -115,21 +112,12 @@ def plot_mesh_tally(
 
     tally_data = tally_slice.get_reshaped_data(expand_dims=True, value=value).squeeze()
 
-    print('tally_data.shape', tally_data.shape)
-
+    # get the middle phi value
     if slice_index is None:
-        basis_to_index = {"rz": 1, "phir": 0}[basis]  # todo check phir
-        slice_index = int(tally_data.shape[basis_to_index] / 2)
+        slice_index = int(tally_data.shape[1] / 2)  # index 1 is the phi value
 
-    if basis == "rz":
-        data = tally_data[:, slice_index, :]
-        # data = np.rot90(slice_data, 1)
-        xlabel, ylabel = f"r [{axis_units}]", f"z [{axis_units}]"
-    else:  # basis == 'phir'
-        # todo
-        pass
+    data = tally_data[:, slice_index, :]
 
-    print('data.shape', data.shape)
     if volume_normalization:
         slice_volumes = mesh.volumes[:, slice_index, :]
         data = data / slice_volumes
@@ -137,15 +125,12 @@ def plot_mesh_tally(
     if scaling_factor:
         data = data * scaling_factor
 
-    if basis == "rz":
-        data = np.rot90(data, 1)
+    data = np.rot90(data, 1)
 
+    xlabel, ylabel = f"r [{axis_units}]", f"z [{axis_units}]"
     axis_scaling_factor = {"km": 0.00001, "m": 0.01, "cm": 1, "mm": 10}[axis_units]
 
-    if basis == "rz":
-        extent = [mesh.r_grid[0], mesh.r_grid[-1], mesh.z_grid[0], mesh.z_grid[-1]]
-    else:
-        raise NotImplementedError("todo extent for phi basis plot")
+    extent = [mesh.r_grid[0], mesh.r_grid[-1], mesh.z_grid[0], mesh.z_grid[-1]]
 
     x_min, x_max, y_min, y_max = [i * axis_scaling_factor for i in extent]
 
@@ -165,49 +150,32 @@ def plot_mesh_tally(
         # code to make sure geometry outline is in the middle of the mesh voxel
         # two of the three dimensions are just in the center of the mesh
         # but the slice can move one axis off the center so this needs calculating
-        x0, y0, z0 = mesh.lower_left
         x1, y1, z1 = mesh.upper_right
-        xo, yo, zo = mesh.origin
-        nx, ny, nz = mesh.dimension
+        x_origin, y_origin, z_origin = mesh.origin
 
-        xc = (xo + x1) / 2
-        yc = (yo + y1) / 2
-        zc = mesh.bounding_box.center[2]
-        print(' mesh.bounding_box',  mesh.bounding_box)
-
-        width_x = (xo+x1)
-        width_y = (yo+y1)
-        width_z = (zo+z1)
-
-        center_of_mesh = mesh.bounding_box.center
-        if basis == "rz":
-            center_of_mesh_slice = [xc, 0., zc]
-        if basis == "phir":
-            raise NotImplementedError("outline with phir basis is not yet supported, contributions are welcome :-)")
-
-        geometry_basis='xz' # TODO allow rz to result in xy or yz
-
-        print('mesh.bounding_box.center', mesh.bounding_box.center)
-        print('center_of_mesh_slice', center_of_mesh_slice)
-        print('mesh.origin', mesh.origin)
-# mesh.bounding_box.center [0. 0. 0.]
-# center_of_mesh_slice [1250.0, 1250.0, 0.0]
-# mesh.origin [0. 0. 0.]
+        width_x = x_origin + x1
+        width_y = y_origin + y1
+        width_z = z_origin + z1
 
         model = openmc.Model()
         model.geometry = geometry
         plot = openmc.Plot()
-        plot.origin = (0.5*width_x,0.,0.5*width_z)#(1250, 0 , 1000)#center_of_mesh_slice
-        # bb_width = mesh.bounding_box.extent[basis]
-        plot.width = (width_x, width_z) # tod might use width y if yz basis used
-        aspect_ratio = width_x / width_y
+
+        if geometry_basis == "xz":
+            plot.origin = (0.5 * width_x, 0.0, 0.5 * width_z)
+            plot.width = (width_x, width_z)
+            aspect_ratio = width_x / width_z
+        else:  # geometry_basis='xz'
+            plot.origin = (0, 0.5 * width_y, 0.5 * width_z)
+            plot.width = (width_y, width_z)
+            aspect_ratio = width_y / width_z
+
         pixels_y = math.sqrt(pixels / aspect_ratio)
         pixels = (int(pixels / pixels_y), int(pixels_y))
         plot.pixels = pixels
         plot.basis = geometry_basis
         plot.color_by = outline_by
         model.plots.append(plot)
-        print(plot)
 
         with TemporaryDirectory() as tmpdir:
             # Run OpenMC in geometry plotting mode
@@ -223,15 +191,7 @@ def plot_mesh_tally(
         rgb = (img * 256).astype(int)
         image_value = (rgb[..., 0] << 16) + (rgb[..., 1] << 8) + (rgb[..., 2])
 
-        if geometry_basis == "xz":
-            pass
-            # image_value = np.rot90(image_value, 2)
-        elif geometry_basis == "yz":
-            image_value = np.rot90(image_value, 2)
-        else:  # geometry_basis == 'xy'
-            image_value = np.rot90(image_value, 2)
-
-        # Plot image and return the axes
+        # Plot geometry image
         axes.contour(
             image_value,
             origin="upper",
